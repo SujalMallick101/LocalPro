554a92f180c7e8acaa119257f44056b5b75756ae
sujal mallick <mallicksujal626@gmail.com>
2025-07-26 15:53:51 +0530

    live

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://localpro-backend.onrender.com",
 })
 
 API.interceptors.request.use((config) => {
