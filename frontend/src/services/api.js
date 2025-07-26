a0cedf83725bd01332d929add1948ce5790e62d5
sujal mallick <mallicksujal626@gmail.com>
2025-07-26 17:57:26 +0530

    rectify the url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 const API = axios.create({
-    baseURL: "https://localpro-backends.onrender.com",
-})
+  baseURL: "http://localhost:5000/api",
+});
 
 API.interceptors.request.use((config) => {
   const user = JSON.parse(localStorage.getItem("user"));
